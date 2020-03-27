f4f52e36d46d49e59b4b9fa2e2fa7562963ed962
Puneeth1996 <puneeth1996p@gmail.com>
2020-03-27 23:07:46 +0530

    Updates

---
+++ precache-manifest.9190d5eaf25963ce07d7f696f327da9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e57f9470050df9279b7548228f89d7",
+    "url": "/contact-friendly-zombies/index.html"
+  },
+  {
+    "revision": "20b9f0ac2f700f107aaa",
+    "url": "/contact-friendly-zombies/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/contact-friendly-zombies/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "20b9f0ac2f700f107aaa",
+    "url": "/contact-friendly-zombies/static/js/main.1d394468.chunk.js"
+  },
+  {
+    "revision": "e3cb9602d58ee0637ce4",
+    "url": "/contact-friendly-zombies/static/js/runtime~main.dc2cbc73.js"
+  }
+]);
