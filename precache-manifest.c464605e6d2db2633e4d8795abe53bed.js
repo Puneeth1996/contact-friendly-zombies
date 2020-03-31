900727a67efcc223b9446a12f22ff8fe17eeefa7
Puneeth1996 <puneeth1996p@gmail.com>
2020-03-31 12:18:33 +0530

    Updates

---
+++ precache-manifest.c464605e6d2db2633e4d8795abe53bed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ecf9891bfbe29b5e9cb00ee5c62b26",
+    "url": "/contact-friendly-zombies/index.html"
+  },
+  {
+    "revision": "004d59106951f4023a02",
+    "url": "/contact-friendly-zombies/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c7dd48f04a2812efe615",
+    "url": "/contact-friendly-zombies/static/css/main.75333c6d.chunk.css"
+  },
+  {
+    "revision": "004d59106951f4023a02",
+    "url": "/contact-friendly-zombies/static/js/2.8ed5472f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/contact-friendly-zombies/static/js/2.8ed5472f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7dd48f04a2812efe615",
+    "url": "/contact-friendly-zombies/static/js/main.fc1175d7.chunk.js"
+  },
+  {
+    "revision": "f87b868035d2366e2d62",
+    "url": "/contact-friendly-zombies/static/js/runtime-main.15bd9774.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/contact-friendly-zombies/static/media/SEGA.12a3db8b.woff"
+  }
+]);
